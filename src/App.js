5d9afc67a2ac854d761e138d1110fbb91590edcd
Shoon Lei May <www.shoonleimay@gmail.com>
2025-02-14 03:04:02 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Memories from "./components/Memories";
+import Proposal from "./components/Proposal";
+import "./App.css";
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <Router basename="/for-only-seunghwan/formybaby">
+      <div className="app-container">
+        <Routes>
+          {/* Login Page */}
+          <Route
+            path="/"
+            element={<Login onLogin={() => setIsAuthenticated(true)} />}
+          />
+
+          {/* Memories Page (Protected) */}
+          <Route
+            path="/memories"
+            element={isAuthenticated ? <Memories /> : <Login onLogin={() => setIsAuthenticated(true)} />}
+          />
+
+          {/* Proposal Page (Protected) */}
+          <Route
+            path="/proposal"
+            element={isAuthenticated ? <Proposal /> : <Login onLogin={() => setIsAuthenticated(true)} />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
